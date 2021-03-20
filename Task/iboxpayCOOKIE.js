b52f407c253aa37d7932238063b4a083dabb7138
NIKE996 <77569083+NIKE996@users.noreply.github.com>
2021-03-20 22:21:35 +0800

    Add files via upload

---
+++ Task/iboxpayCOOKIE.js
@@ -0,0 +1,26 @@
+
+
+//独立COOKIE文件     ck在``里面填写，多账号换行
+
+let refreshtokenVal= `5a2449f7941144fcbe31fa0001db949c
+                                     0d89745052564fd59dae6bbb319ffd18
+                                     88966396cd5343618029912f922fa531
+                                     f281ce932f04459682b70286b66bb09c
+                                     890e5aa36e4b4fddbcf20a534fc3c943
+                                     bace521956c8413cb5ef3dbf904f53bc
+                                     769417ae37bd41369ccdb2731fa9902e
+                                     8636941d5b864dee9a870b0361e8d321
+                                     2ea13890147049b0bc01e1040f4a90d7
+                                     07ef1de5399b4227baa691983f098dbc`
+
+
+let iboxpaycookie = {
+
+  refreshtokenVal: refreshtokenVal,  
+  
+}
+
+module.exports =  iboxpaycookie
+  
+
+

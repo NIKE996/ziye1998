dfded9c6509b62c0499e76263a83a63fd4d1f0ff
NIKE996 <77569083+NIKE996@users.noreply.github.com>
2021-03-21 12:07:34 +0800

    Add files via upload

---
+++ Task/QQreadCOOKIE.js
@@ -0,0 +1,19 @@
+//独立COOKIE文件     ck在``里面填写，多账号换行
+let QQreadurlVal = `https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywRQFORFOe1S&ywguid=2015662961
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywZS4ZHVfeOW&ywguid=120197212134&usid=43_wKydvY0piN0ww-Wu1oL4qz7pZ8d3U8g1db0l3xxEsHgc0tjh6ZbqMbJtTDJDiSA1MUmCloc-qzJwDzn842Ful0SNfU1VwjxAhD3Iz1fju8A
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywQLjUJjuEPN&ywguid=2111601711
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywQGxMAUfCxt&ywguid=3502208249
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=yw1pQCBhJy8q&ywguid=639613242
+https://iostgw6.reader.qq.com/v7_5_7/userinfo?ywkey=ywEiEejtA0GG&ywguid=639559338
+https://newios.reader.qq.com/v7_5_7/userinfo?ywkey=ywQJXJcRCC1I&ywguid=862460781`
+
+
+
+
+let QQreadurlcookie = {
+
+    QQreadurlVal: QQreadurlVal,
+
+}
+
+module.exports = QQreadurlcookie
